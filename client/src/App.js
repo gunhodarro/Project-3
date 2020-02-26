bbfca7e64fc556adfca5de315561826fde97fe33
tim <timothy.potts@colorado.edu>
2020-02-26 16:45:13 -0700

    routing operational

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import OurMission from "./pages/OurMission"
-// import SignIn from "./pages/SignIn"
-// import Give from "./pages/Give"
-// import Receive from "./pages/Receive"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import OurMission from "./pages/OurMission";
+import SignIn from "./pages/SignIn";
+import Give from "./pages/Give";
+import Receive from "./pages/Receive";
 
 import "./App.css";
 
-
 function App() {
   return (
-    <div>
-      <Home />
-      {/* <Give /> */}
-      {/* <OurMission /> */}
-      {/* <Receive /> */}
-      {/* <Signin /> */}
-    
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={Home} />
+        <Route path="/give" component={Give} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/mission" component={OurMission} />
+        <Route path="/receive" component={Receive} />
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
