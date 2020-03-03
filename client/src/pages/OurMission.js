da2a82bdfef65f6ef32bc895e7b3a02908db18c1
Don Dean <Dondean3030@gmail.com>
2020-03-02 19:33:48 -0700

    added to ourMission tab

--- client/src/pages/OurMission.js
+++ client/src/pages/OurMission.js
@@ -9,6 +9,10 @@ function OurMission() {
         <>
         <NavBar />
          <Header />
+         <p>With so much food being wasted in the US alone, we are striving to connect unused and unsold food from restaurants to non profits that can then redistribute 
+             to hungry people.
+         </p>
+         <img src="https://images.unsplash.com/flagged/photo-1570088782323-e49798c279f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" ></img>
          <Footer />
         
         </>
