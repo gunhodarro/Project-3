0597dfc6dc27fce3eede417f1d4e2c55393fac98
tim <timothy.potts@colorado.edu>
2020-03-09 20:41:47 -0600

    closer

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+var db = require("./models");
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
